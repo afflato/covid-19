d202eb8211a8d9b269e17aefc5473b258a240370
Sharad Paul <paul.sharadchandra@gmail.com>
2020-05-13 22:50:45 +0530

    Updates

---
+++ precache-manifest.d8c04651e62b2b531078f69160b80885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caaecc2225a4afa5e0d81cc0c02a0ab8",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "03b2c0744a178d3e10e9",
+    "url": "/covid-19/static/css/main.5ce73658.chunk.css"
+  },
+  {
+    "revision": "621715e88503e2cf8505",
+    "url": "/covid-19/static/js/2.e266d60d.chunk.js"
+  },
+  {
+    "revision": "03b2c0744a178d3e10e9",
+    "url": "/covid-19/static/js/main.93561c58.chunk.js"
+  },
+  {
+    "revision": "addb358ac25b5163254c",
+    "url": "/covid-19/static/js/runtime~main.f4371696.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
